cdb794051d8a784a2bc824a91ec65d901b3a43d8
labuda.jakub@gmail.com <labuda.jakub@gmail.com>
2018-03-02 16:49:10 +0100

    regex lookup

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-usage:
-
-ruby filter.rb <search_term> <some_facebook_page> <results_quantity> <scrolls_till_browser_restarts>
-
-
-
-exemplary usages:
-
-ruby filter.rb konkurs https://fb.com/ 2 200
-
-ruby filter.rb 'w krakowie' https://www.facebook.com/sledzimykonkursy/ 3 1000
-
-
-
-requirements:
-
-ruby, selenium-webdriver gem, geckodriver in PATH, firefox with saved fb password
+usage:  
+ruby filter.rb <case_insensitive_regex_search_term> <some_facebook_page> <results_quantity> <scrolls_till_browser_restarts>  
+  
+exemples:  
+ruby filter.rb Youtube https://fb.com/ 3 100  
+ruby filter.rb 'piwo|wódka|wino' https://facebook.com/ 1 100  
+ruby filter.rb '(?=.*Duda)(?=.*Andrzej)' https://www.fb.com/ 1 100  
+ruby filter.rb 'wygraJ bileT' https://www.facebook.com/sledzimykonkursy/ 1 1000  
+  
+requirements:  
+ruby, selenium-webdriver gem, geckodriver in PATH, firefox with saved fb password  
--- filter.rb
+++ filter.rb
@@ -3,7 +3,7 @@ require 'selenium-webdriver'
 def homepage(link,hide)
   default_profile = Selenium::WebDriver::Firefox::Profile.from_name 'default' #run "firefox -P" to check profile name
   options = Selenium::WebDriver::Firefox::Options.new(profile: default_profile)
-  options.add_argument('--headless') if hide==1 #hide UI, faster
+  options.add_argument('--headless') if hide == 1 #hide UI, faster
   $browser = Selenium::WebDriver.for :firefox, options: options
   $browser.manage.window.maximize
   $browser.get link
@@ -12,17 +12,17 @@ end
 output = []
 puts "lets look for #{ARGV[0]}..."
 while output.length < ARGV[2].to_i
-  i=0
+  i = 0
   homepage(ARGV[1],1)
-  while i<ARGV[3].to_i && (output.length < ARGV[2].to_i)
-    $browser.find_elements(xpath: "//div[@role='article']").each do |el|
-      if el.attribute('textContent').downcase.include? ARGV[0].downcase
+  while i < ARGV[3].to_i && output.length < ARGV[2].to_i
+    $browser.find_elements(xpath: "//div[@role='article' and .//input[contains(@name,'ft_ent_identifier')]]").each do |el|
+      if !(el.attribute('textContent') =~ /#{ARGV[0]}/i).nil? && output.length < ARGV[2].to_i
         url = "https://www.facebook.com/"+el.find_element(xpath: ".//input[@name='ft_ent_identifier']").attribute('value')
         output << url if !output.include? url
       end
     end
     $browser.execute_script("window.scrollBy(0, window.innerHeight)")
-    i+=1
+    i += 1
     print (ARGV[3].to_i-i).to_s+", " #show how many scrolls left to browser restart
   end
   $browser.quit
@@ -32,5 +32,5 @@ end
 p output #show collected urls
 homepage(output[0],0)
 output.each_with_index do |s,index| 
-  $browser.execute_script("window.open(\"#{s}\")") if index!=0
+  $browser.execute_script("window.open(\"#{s}\")") if index != 0
 end
